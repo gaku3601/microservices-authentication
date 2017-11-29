c22c68e7b008759a8ecc4efe2b759121527a2808
gaku0601 <pro.gaku@gmail.com>
2017-11-29 19:48:45 +0900

    testコードをリファクタリング

--- authentication/models/user_test.go
+++ authentication/models/user_test.go
@@ -6,36 +6,38 @@ import (
 )
 
 //emailとpasswordがDBにUser登録されていることを確認する。
-func TestFetchUserCheck(t *testing.T) {
+func TestNewUser(t *testing.T) {
 	//userにemailとpasswordを渡し、格納されていることを確認する。
 	user := NewUser("pro.gaku@gmail.com", "password")
-	if user.email == "pro.gaku@gmail.com" {
-	} else {
+	if user.email != "pro.gaku@gmail.com" {
 		t.Error("useremail格納エラー")
 	}
-	if user.password == "password" {
-	} else {
+
+	if user.password != "password" {
 		t.Error("userpass格納エラー")
 	}
 
+}
+
+func TestMD5hash(t *testing.T) {
 	//passwordをハッシュ化する
-	hashpass1 := user.md5hash("password")
-	if len(hashpass1) == 32 {
-	} else {
+	user := NewUser("pro.gaku@gmail.com", "password")
+	hashdata := user.md5hash("password")
+
+	if len(hashdata) != 32 {
 		t.Log(user.md5hash("password"))
 		t.Error("パスワードハッシュ化エラー")
 	}
-	hashpass2 := user.md5hash("password2")
-	if hashpass1 != hashpass2 {
-	} else {
+	if user.md5hash("password2") == hashdata {
 		t.Error("正常にハッシュ化されていません。")
 	}
+}
 
+func TestDBConnect(t *testing.T) {
 	//DB接続を行う
+	user := NewUser("pro.gaku@gmail.com", "password")
 	user.dbConnect(func(db *sql.DB) {
-		err := db.Ping()
-		if err == nil {
-		} else {
+		if db.Ping() != nil {
 			t.Error("DB接続エラー")
 		}
 	})
